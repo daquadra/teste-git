dbedd5d828ba1ba03846527be2799d975d422848
Madson Lima <madson@netpix.com.br>
2016-12-09 17:09:47 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Curso GIT</title>
+</head>
+<body>
+
+
+<section>
+    <ul>
+        <li>Nome: <a href="#">Madson Lima</a></li>
+        <li>Profissão: <a href="#">Programador PHP</a></li>
+    </ul>
+</section>
+
+
+</body>
+</html>

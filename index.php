19f6e8d3f741196d50e35ec6a7f0ad2bbf847da2
Madson Lima <madson@netpix.com.br>
2016-12-13 10:12:39 -0400

    Adicionando estilo inicial

--- index.php
+++ index.php
@@ -6,15 +6,17 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Curso GIT</title>
+    <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
 
 
 <section>
+    <h1>Dados pessoais:</h1>
     <ul>
         <li>Nome: <a href="#">Madson Lima</a></li>
         <li>Profissão: <a href="#">Programador PHP</a></li>
-        <li>Civil: <a href="#">Casado</a></li>
+        <li>Estado Civil: <a href="#">Casado</a></li>
     </ul>
 </section>
 
